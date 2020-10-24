0863f527947ad2b896e811da8f9df176508d0b60
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-10-24 20:09:01 +0200

    Created Home, Login, Logout and Register containers. Installed Axios and Ant Design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { Switch, BrowserRouter/*, Route*/ } from 'react-router-dom'
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
+import 'antd/dist/antd.css';
 
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+
+import Register from './containers/Register/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
+        <Route path ='/register' component={Register}/>
        </Switch>
        <Footer />
        </BrowserRouter>
---
+++ src/containers/Register/Register.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {useHistory} from "react-router";
+
+import {Input, Button, notification } from 'antd';
+import axios from 'axios';
+
+const Register = () =>{
+
+    const history = useHistory();
+
+    const handleSubmit = event =>{
+        event.preventDefault(); // Prevent the page from refreshing.
+        const userBody={
+            name: event.target.name.value,
+            phone: event.target.phone.value,
+            email: event.target.email.value,
+            password: event.target.password.value,
+            age: event.target.age.value,
+            address: event.target.address.value,
+            nationality: event.target.nationality.value,
+            DNI: event.target.DNI.value
+        };
+        
+        axios.post('http://localhost:3001/client/register', userBody)
+        .then(res => {
+            console.log(res.data)
+            notification.success({ message :'Registered client.',description:'Succesfully registered client.'})
+            
+            setTimeout(() => {
+                history.push("/")
+            }, 1500);
+        }).catch(error => {
+            notification.error({ message: 'Registration error.', description: 'There was an error trying to register the client.' })
+        })
+    }
+
+    return (
+        <form className="register-form" onSubmit={handleSubmit}>
+
+        <Input type="name" name="name" required placeholder="Write your name" />
+        <Input type="phone" name="phone" required placeholder="Write your phone" />
+        <Input type="email" name="email" required placeholder="Write your email" />
+        <Input type="password" name="password" required placeholder="Write your password" />
+        <Input type="age" name="age" required placeholder="Write your age" />
+        <Input type="address" name="address" required placeholder="Write your address" />
+        <Input type="nationality" name="nationality" required placeholder="Write your nationality" />
+        <Input type="DNI" name="DNI" required placeholder="Write your DNI" />
+
+
+
+
+        <Button type="primary" htmlType="submit">Sign up</Button>
+
+    </form>
+)
+}
+
+export default Register;
