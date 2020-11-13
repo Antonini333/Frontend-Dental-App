b05dd4b44959652c4fcaec3e0a93514aa3d26a75
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-11-13 12:49:29 +0100

    Deploying

---
+++ build/precache-manifest.de7368a00b553095cf16fd635145dce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4cfca56b08eaaa3e00764d5f4d439cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d8ed4c376485f3357a",
+    "url": "/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "c8d890d96d4badd61e23",
+    "url": "/static/css/main.ac22f004.chunk.css"
+  },
+  {
+    "revision": "72d8ed4c376485f3357a",
+    "url": "/static/js/2.4a684366.chunk.js"
+  },
+  {
+    "revision": "a07f0d37bad529c29913b02917e371bc",
+    "url": "/static/js/2.4a684366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d890d96d4badd61e23",
+    "url": "/static/js/main.1c9a80ec.chunk.js"
+  },
+  {
+    "revision": "75aa751975cb0dff309a",
+    "url": "/static/js/runtime-main.740667d5.js"
+  }
+]);
